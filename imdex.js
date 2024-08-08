becb34f496566e937f8dc6d97562ceb5abdf3a0c
Daniyar410 <ramziya.shakirova@gmail.com>
2024-08-08 15:30:07 +0500

    Add files via upload

--- imdex.js
+++ imdex.js
@@ -2,17 +2,28 @@
 let car = {
     brand: 'BMW',
     model: 'm5cs',
-    released: 15 ,
+    released: 15,
 
 }
 
 car.color = 'blak'
-car.released = 12 
-
-
+car.released = 12
 
 
 console.log(car);
 
 
 
+let humans = {
+    name: 'Daniyar',
+    surname: 'Shakirov',
+    age: 16
+}
+
+humans.name = 'John'
+humans.surname = 'Wick'
+humans.age = 33
+
+let humans2 = {...humans}
+
+console.log(humans2);
